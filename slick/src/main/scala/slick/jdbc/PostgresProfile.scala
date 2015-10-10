ce4b67c76e3e283c2a0d614cc95ae65c0e82a732
AtkinsChang <atkinschang@gmail.com>
2015-10-11 04:52:03 +0800

    fix ModelBuilder for postgres 9.4

--- slick/src/main/scala/slick/jdbc/PostgresProfile.scala
+++ slick/src/main/scala/slick/jdbc/PostgresProfile.scala
@@ -64,13 +64,16 @@ trait PostgresProfile extends JdbcProfile {
     }
     override def createColumnBuilder(tableBuilder: TableBuilder, meta: MColumn): ColumnBuilder = new ColumnBuilder(tableBuilder, meta) {
       val VarCharPattern = "^'(.*)'::character varying$".r
-      val IntPattern = "^\\((-?[0-9]*)\\)$".r
+      val IntPattern = "^['(](-?[0-9]*)[)'](::[a-z ]*)?$".r
+      val FloatPattern = "^['(](-?[0-9]*.[0-9]*)[)'](::[a-z ]*)?$".r
       override def default = meta.columnDef.map((_,tpe)).collect{
         case ("true","Boolean")  => Some(Some(true))
         case ("false","Boolean") => Some(Some(false))
         case (VarCharPattern(str),"String") => Some(Some(str))
-        case (IntPattern(v),"Int") => Some(Some(v.toInt))
-        case (IntPattern(v),"Long") => Some(Some(v.toLong))
+        case (IntPattern(v, _),"Int") => Some(Some(v.toInt))
+        case (IntPattern(v, _),"Long") => Some(Some(v.toLong))
+        case (FloatPattern(v, _),"Float") => Some(Some(v.toFloat))
+        case (FloatPattern(v, _),"Double") => Some(Some(v.toDouble))
         case ("NULL::character varying","String") => Some(None)
         case (v,"java.util.UUID") => {
           val uuid = v.replaceAll("[\'\"]", "") //strip quotes
